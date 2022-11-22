28bb0b5ccd01f648169ffa23137a17cef2b741c6
shoshlavosh <shoshlavosh@gmail.com>
2022-11-22 15:30:31 -0800

    calculator.py extra functions complete

--- arithmetic.py
+++ arithmetic.py
@@ -47,3 +47,15 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+
+def add_mult(num1, num2, num3):
+    """Return the sum of (num 1 + num2) multiplied by num3 """
+
+    return (num1 + num2) * num3
+
+
+def add_cubes(num1, num2):
+    """Return the sum of the cubes of num1 and num2"""
+
+    return cube(num1) + cube(num2)
